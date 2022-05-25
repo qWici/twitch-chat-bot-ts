a4e265fe11d22cd1bb353f1c6e6dc8e87aa4cfd6
Ivan Kucher <ivan.kucher@gen.tech>
2022-05-25 17:49:16 +0300

    refactor(social-links): export Command instance as variable not function

--- src/commands/social-links.ts
+++ src/commands/social-links.ts
@@ -2,19 +2,17 @@ import { Command } from "@common/command";
 import { CommandResolver } from "../types";
 import { Signatures } from "./signatures";
 
-export const SocialLinks = () => {
-  const resolver: CommandResolver = (args) => {
-    if (!args || !args.type) return false;
+const resolver: CommandResolver = (args) => {
+  if (!args || !args.type) return false;
 
-    switch (args.type) {
-      case "fb":
-        return "facebook.com";
-      case "tw":
-        return "tw.com";
-      default:
-        return false;
-    }
-  };
-
-  return new Command({ name: Signatures.SOCIAL_LINKS, resolver, args: [{ key: "type", type: "string" }] });
+  switch (args.type) {
+    case "fb":
+      return "facebook.com";
+    case "tw":
+      return "tw.com";
+    default:
+      return false;
+  }
 };
+
+export const SocialLinks = new Command({ name: Signatures.SOCIAL_LINKS, resolver, args: [{ key: "type", type: "string" }] });
